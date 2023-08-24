42270424f7bcbc87e1e6e5b0d05b8034ffc151e8
Mohamed26Salah <76624455+Mohamed26Salah@users.noreply.github.com>
2023-08-24 22:11:22 +0300

    added Custom Radio Button

--- BM-Currency-Graduation/BM-Currency-Graduation/View/Customs/RadioButton.swift
+++ BM-Currency-Graduation/BM-Currency-Graduation/View/Customs/RadioButton.swift
@@ -8,6 +8,14 @@
 import UIKit
 
 class RadioButton: UIButton {
+    var isChecked: Bool = false {
+        didSet {
+            updateButtonAppearance()
+        }
+    }
+    
+    private let feedbackGenerator = UIImpactFeedbackGenerator(style: .light)
+    
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
         self.initButton()
@@ -22,9 +30,42 @@ class RadioButton: UIButton {
         self.backgroundColor = .clear
         self.tintColor = .clear
         self.setTitle("", for: .normal)
-        self.setImage(UIImage(named: "radio_button_unchecked")?.withRenderingMode(.alwaysOriginal), for: .normal)
-        self.setImage(UIImage(named: "radio_button_checked")?.withRenderingMode(.alwaysOriginal), for: .highlighted)
-        self.setImage(UIImage(named: "radio_button_checked")?.withRenderingMode(.alwaysOriginal), for: .selected)
+        updateButtonAppearance()
+        self.addTarget(self, action: #selector(buttonTapped), for: .touchUpInside)
     }
-
+    
+    @objc func buttonTapped() {
+        isChecked = !isChecked
+        feedbackGenerator.impactOccurred()
+    }
+    
+    func updateButtonAppearance() {
+        if isChecked {
+            self.setImage(UIImage(named: "radio_button_checked")?.withRenderingMode(.alwaysOriginal), for: .normal)
+        } else {
+            self.setImage(UIImage(named: "radio_button_unchecked")?.withRenderingMode(.alwaysOriginal), for: .normal)
+        }
+    }
 }
+
+//class RadioButton: UIButton {
+//    required init?(coder aDecoder: NSCoder) {
+//        super.init(coder: aDecoder)
+//        self.initButton()
+//    }
+//
+//    override init(frame: CGRect) {
+//        super.init(frame: frame)
+//        self.initButton()
+//    }
+//
+//    func initButton() {
+//        self.backgroundColor = .clear
+//        self.tintColor = .clear
+//        self.setTitle("", for: .normal)
+//        self.setImage(UIImage(named: "radio_button_unchecked")?.withRenderingMode(.alwaysOriginal), for: .normal)
+//        self.setImage(UIImage(named: "radio_button_checked")?.withRenderingMode(.alwaysOriginal), for: .highlighted)
+//        self.setImage(UIImage(named: "radio_button_checked")?.withRenderingMode(.alwaysOriginal), for: .selected)
+//    }
+//
+//}

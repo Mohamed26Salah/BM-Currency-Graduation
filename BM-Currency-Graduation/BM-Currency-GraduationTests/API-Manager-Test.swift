891e31dcfc1147af5101be316999cf9c681fa509
Mohamed26Salah <76624455+Mohamed26Salah@users.noreply.github.com>
2023-08-29 03:12:00 +0300

    Added Three Test Cases

--- BM-Currency-Graduation/BM-Currency-GraduationTests/API-Manager-Test.swift
+++ BM-Currency-Graduation/BM-Currency-GraduationTests/API-Manager-Test.swift
@@ -9,41 +9,112 @@ import XCTest
 import RxSwift
 import RxRelay
 import RxCocoa
+import RxTest
 
 @testable import BM_Currency_Graduation
 
 final class API_Manager_Test: XCTestCase {
-    let disposeBag = DisposeBag()
-
-    var apiManager: APIManager!
+    var disposeBag: DisposeBag!
+    var scheduler: TestScheduler!
+    var networkService: APIManager! // Replace with your network service protocol
     
-    override func setUpWithError() throws {
-        apiManager = APIManager.shared()
+    override func setUp() {
+        super.setUp()
+        disposeBag = DisposeBag()
+        scheduler = TestScheduler(initialClock: 0)
+        networkService = APIManager.shared() // Replace with your network service implementation
     }
-
-    override func tearDownWithError() throws {
-       apiManager = nil
-    }
-
-    func testAPIManagerSuccessful() {
-        let promise = XCTestExpectation(description: "Done")
-        var responseError: Error?
-        var allCurrencies: AllCurrencies?
-        guard let bundle = Bundle.unitTest.path(forResource: "stub", ofType: "json") else {
-            XCTFail("Fail")
-            return
-        }
-        apiManager.fetchGlobal(parsingType: AllCurrencies.self, baseURL: URL(fileURLWithPath: bundle))
-            .subscribe(onNext: { currencyArray in
-                allCurrencies = currencyArray
-            }, onError: { error in
-                responseError = error
-            })
-            .disposed(by: disposeBag)
-        promise.fulfill()
-        wait(for: [promise], timeout: 1)
-        XCTAssertNil(responseError)
-        XCTAssertNotNil(allCurrencies)
-    }
+    
+//    func testFetchGlobal_Success() {
+//        // Given
+//        let bundle = Bundle(for: type(of: self))
+//        guard let jsonPath = bundle.path(forResource: "stub", ofType: "json"),
+//              let jsonData = try? Data(contentsOf: URL(fileURLWithPath: jsonPath)),
+//              let expectedData = try? JSONDecoder().decode(AllCurrencies.self, from: jsonData) else {
+//            XCTFail("Failed to load stub JSON or decode it")
+//            return
+//        }
+//        
+//        let mockSession = MockURLSession()
+//        let networkService = YourNetworkServiceImplementation(session: mockSession)
+//        
+//        mockSession.mockResponse = MockURLSession.mockResponse(statusCode: 200, data: jsonData)
+//        
+//        // When
+//        let fetchObservable = networkService.fetchGlobal(parsingType: YourCodableType.self, baseURL: URL(string: "https://example.com")!)
+//        
+//        // Then
+//        let observer = scheduler.createObserver(YourCodableType.self)
+//        
+//        scheduler.scheduleAt(0) {
+//            fetchObservable
+//                .subscribe(observer)
+//                .disposed(by: self.disposeBag)
+//        }
+//        
+//        scheduler.start()
+//        
+//        XCTAssertEqual(observer.events, [.next(0, expectedData)])
+//    }
 
 }
+
+protocol URLSessionProtocol {
+    func dataTask(with request: URLRequest, completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) -> URLSessionDataTaskProtocol
+}
+
+protocol URLSessionDataTaskProtocol {
+    func resume()
+}
+
+class MockURLSessionDataTask: URLSessionDataTaskProtocol {
+    private let completionHandler: (Data?, URLResponse?, Error?) -> Void
+    
+    init(data: Data?, response: URLResponse?, error: Error?, completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) {
+        self.completionHandler = completionHandler
+    }
+    
+    func resume() {
+        completionHandler(nil, nil, nil) // Call the completion handler with your mock data, response, and error
+    }
+}
+
+class MockURLSession: URLSessionProtocol {
+    var mockResponse: (data: Data?, response: URLResponse?, error: Error?)?
+    
+    func dataTask(with request: URLRequest, completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) -> URLSessionDataTaskProtocol {
+        let data = mockResponse?.data
+        let response = mockResponse?.response
+        let error = mockResponse?.error
+        
+        return MockURLSessionDataTask(data: data, response: response, error: error, completionHandler: completionHandler)
+    }
+}
+
+extension MockURLSession {
+    static func mockResponse(statusCode: Int, data: Data? = nil) -> HTTPURLResponse {
+        return HTTPURLResponse(url: URL(string: "https://concurrency-api.onrender.com/api/v1/currencies")!, statusCode: statusCode, httpVersion: nil, headerFields: nil)!
+    }
+}
+
+
+//func testAPIManagerSuccessful() {
+//    let promise = XCTestExpectation(description: "Done")
+//    var responseError: Error?
+//    var allCurrencies: AllCurrencies?
+//    guard let bundle = Bundle.unitTest.path(forResource: "stub", ofType: "json") else {
+//        XCTFail("Fail")
+//        return
+//    }
+//    apiManager.fetchGlobal(parsingType: AllCurrencies.self, baseURL: URL(fileURLWithPath: bundle))
+//        .subscribe(onNext: { currencyArray in
+//            allCurrencies = currencyArray
+//        }, onError: { error in
+//            responseError = error
+//        })
+//        .disposed(by: disposeBag)
+//    promise.fulfill()
+//    wait(for: [promise], timeout: 1)
+//    XCTAssertNil(responseError)
+//    XCTAssertNotNil(allCurrencies)
+//}
---
+++ BM-Currency-Graduation/BM-Currency-GraduationTests/Converstion_Test.swift
@@ -0,0 +1,74 @@
+//
+//  Converstion_Test.swift
+//  BM-Currency-GraduationTests
+//
+//  Created by Mohamed Salah on 29/08/2023.
+//
+
+import XCTest
+@testable import BM_Currency_Graduation
+
+final class Converstion_Test: XCTestCase {
+    
+    var currencyVM: CurrencyViewModel!
+    
+    override func setUpWithError() throws {
+        currencyVM = CurrencyViewModel()
+    }
+    
+    override func tearDownWithError() throws {
+        currencyVM = nil
+    }
+    
+    func testFillDropDown() {
+        // Given
+        let currencyArray: AllCurrencies = [
+            CurrenciesArray(name: "United States Dollar", code: "USD", iconURL: ""),
+            CurrenciesArray(name: "Euro", code: "EUR", iconURL: "")
+            // ... Add more currencies
+        ]
+        
+        let expectedOutput = [
+            " 🇺🇸USD",
+            " 🇪🇺EUR",
+        ]
+        
+        // When
+        let result = currencyVM.fillDropDown(currencyArray: currencyArray)
+        
+        // Then
+        XCTAssertEqual(result, expectedOutput)
+    }
+    
+    func testGetFlagEmoji() {
+        // Given
+        let flag = "USD"
+        let expectedEmoji = "🇺🇸"
+        
+        // When
+        let emoji = currencyVM.getFlagEmoji(flag: flag)
+        
+        // Then
+        XCTAssertEqual(emoji, expectedEmoji)
+    }
+    
+    func testCalculateConvertedAmount() {
+        // Given
+        let baseAmount = 100.0
+        let targetCurrency = "EUR"
+        let conversionRates: [String: Double] = [
+            "USD": 0.85,
+            "EUR": 1.0,
+            // ... Add more conversion rates
+        ]
+        
+        let expectedConvertedAmount = 100.0 * 1.0 // Since targetCurrency is "EUR"
+        
+        // When
+        let convertedAmount = currencyVM.calculateConvertedAmount(baseAmount: baseAmount, targetCurrency: targetCurrency, conversionRates: conversionRates)
+        
+        // Then
+        XCTAssertEqual(convertedAmount, expectedConvertedAmount)
+    }
+    
+}
